b5a92330ded4423ed9cb8b13baeaf74d9e0dc68f
Connor Schweighöfer <connor.schweighoefer@adesso.de>
2026-01-21 10:53:16 +0100

    Fixed Aspect registration

--- src/main/java/me/squidxtv/sip/protection/ScaleInProtectionAspect.java
+++ src/main/java/me/squidxtv/sip/protection/ScaleInProtectionAspect.java
@@ -3,9 +3,15 @@ package me.squidxtv.sip.protection;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class ScaleInProtectionAspect {
+
+    private static final Logger log = LoggerFactory.getLogger(ScaleInProtectionAspect.class);
 
     private final ScaleInProtectionManager protectionManager;
 
@@ -15,12 +21,14 @@ public class ScaleInProtectionAspect {
 
     @Around("@annotation(ProtectedFromScaleIn)")
     public Object aroundProtectedMethod(ProceedingJoinPoint pjp) throws Throwable {
+        log.info("Entering aspect");
         protectionManager.enter();
 
         try {
             return pjp.proceed();
         } finally {
             protectionManager.exit();
+            log.info("Exiting aspect");
         }
     }
 
